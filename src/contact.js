254541b2a6bdb289246d037bb31f3d552a721039
Crab <crabcodes28@gmail.com>
2023-06-20 16:36:59 +0200

    Add contact page

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,24 @@
 function createContactContent() {
     const content = document.getElementById('content');
     
-    content.innerHTML = `Contact Stuff Here!`
+    content.innerHTML = 
+        `<div id="contact">
+            <h2>Contact</h2>
+            <div class="contact-items">
+                <div class="contact-item">
+                    <h4>Fish Splatter Contact Details</h4>
+                    <h6>Chef's Number: 000 000 0000</h6>
+                    <h6>Manager's Number: 000 000 0000</h6>
+                    <h6>Company Email: fakefishsplatter@gmail.com</h6>
+                </div>
+                <div class="contact-item">
+                    <h4>Fish Splatter Locations</h4>
+                    <h6>Upside Hill - Lighthouse Bay</h6>
+                    <h6>Three Hills Down - Fisherman's Bay</h6>
+                    <h6>Sunny Hill Side - Fishter's Port</h6>
+                </div>
+            </div>
+        </div>`
 }
 
 export { createContactContent }
