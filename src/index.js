8ab1f6f0c8708d76a487d892966573c01404f700
Crab <crabcodes28@gmail.com>
2023-06-20 15:28:05 +0200

    Add link functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import { createHomeContent } from './home';
 import { createMenuContent } from './menu';
 import { createContactContent } from './contact';
 
+function createContent(linkTextContent) {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+
+    if (linkTextContent === 'Home') {
+        createHomeContent();
+    }
+    else if (linkTextContent === 'Menu') {
+        createMenuContent();
+    }
+    else if (linkTextContent === 'Contact') {
+        createContactContent();
+    }
+}
+
+function createLinkListeners() {
+    const links = document.getElementsByClassName('list-link');
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', () => {
+            createContent(links[i].textContent);
+        });
+    }
+}
+
+function initializeSite() {
+    createHomeContent();
+    createLinkListeners();
+}
 
 
-createHomeContent();
+initializeSite();
